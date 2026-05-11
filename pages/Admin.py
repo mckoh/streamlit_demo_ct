12d722e06b2dcce2e15266f40346ec8029194302
mckoh <michael.kohlegger@gmail.com>
2026-05-11 13:50:23 +0200

    added regular and admin interface

--- pages/Admin.py
+++ pages/Admin.py
@@ -11,3 +11,13 @@ if "quests" not in st.session_state:
     st.session_state["quests"] = []
 
 st.title("Admin Screen")
+
+if st.button("Reload", key="my_quests_reload"):
+    st.rerun()
+st.markdown("---")
+for i, quest in enumerate(st.session_state["quests"]):
+    st.subheader(quest["subject"])
+    st.markdown(quest["description"])
+    if st.button("Delete Quest forever", key="all"+str(i)):
+        st.session_state["quests"].remove(quest)
+    st.markdown("---")
--- pages/Quests.py
+++ pages/Quests.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import date as dt
 
 st.set_page_config(
     page_icon="🗺️",
@@ -23,15 +24,19 @@ tab1, tab2, tab3 = st.tabs([
 ])
 
 with tab2:
-    if st.button("Reload"):
+    if st.button("Reload", key="quests_reload"):
         st.rerun()
     st.header("Alle Quests")
+    st.markdown("Unterhalb findest du eine Liste mit Challenges, die du absolvieren kannst. Wähle eine Challenge aus, indem du auf den Button klickst.")
+    st.markdown("---")
 
     for i, quest in enumerate(st.session_state["quests"]):
-        st.write(quest["subject"])
-        if st.button("Join this Quest", key=str(i)):
-            st.session_state["my_quests"].append(i)
-        st.markdown("---")
+        if quest["date"] > dt.today():
+            st.subheader(quest["subject"])
+            st.markdown(quest["description"])
+            if st.button("Join this Quest", key=str(i)):
+                st.session_state["my_quests"].append(i)
+            st.markdown("---")
 
 with tab1:
     st.header("Neue Quests")
@@ -51,5 +56,15 @@ with tab1:
         st.write(f"Neue Challenge hinzugefügt: {subject}")
 
 with tab3:
+    if st.button("Reload", key="my_quests_reload"):
+        st.rerun()
     st.header("Meine Quests")
-    st.write(list(set(st.session_state["my_quests"])))
+    st.markdown("An folgenden Quests nimmst du aktuell teil.")
+    st.markdown("---")
+    for i in st.session_state["my_quests"]:
+        quest = st.session_state["quests"][i]
+        st.subheader(quest["subject"])
+        st.markdown(quest["description"])
+        if st.button("Join this Quest", key="my"+str(i)):
+            st.session_state["my_quests"].remove(i)
+        st.markdown("---")
